9c1e3f444a7ffb6b3468eed4e643a47a4788851d
Chuanjian Wang <me@ckeyer.com>
2016-12-15 22:56:37 +0800

    init

---
+++ src/components/account/SignUp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="signup">
+    SignUp
+    <div class="form-group">
+      <label class="col-sm-2 control-label">api root path</label>
+      <div class="col-sm-10">{{apiPath}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signup',
+  data () {
+    return {
+      apiPath: process.env.API_ROOT
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
